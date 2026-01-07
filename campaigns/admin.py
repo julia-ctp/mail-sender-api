7c09940be475ec11c52c8ccc51c553573e807348
julia-ctp <juliactp.santos@gmail.com>
2026-01-07 14:54:24 -0300

    chore: improve user admin display and register contact and campaign models

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Campaign, CampaignContact
 
-# Register your models here.
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("sender_email", "subject", "status", "created_at")
+
+@admin.register(CampaignContact)
+class CampaignContactAdmin(admin.ModelAdmin):
+    list_display = ("campaign", "contact", "created_at", "status")
