aac536824e279a583ba9fad01888b1ac97bf2ea9
julia-ctp <juliactp.santos@gmail.com>
2026-01-07 14:19:50 -0300

    feat: improve user model with unique email and timestamps

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,13 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser): 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    def __str__(self): 
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
         return self.email
