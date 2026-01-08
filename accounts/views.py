a73e617a63771f1c2dea15b8c2054184aea209d5
julia-ctp <juliactp.santos@gmail.com>
2026-01-07 21:27:55 -0300

    feat: add authentication system with jwt

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import (
+    UserSerializer, 
+    UserRegisterSerializer, 
+    EmailTokenObtainPairSerializer
+)
 
-# Create your views here.
+
+class UserRegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+
+class LoginView(TokenObtainPairView):
+    serializer_class = EmailTokenObtainPairSerializer
--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth', include('accounts.urls')),
-    path('api/contacts', include('contacts.urls')),
-    path('api/campaigns', include('campaigns.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/contacts/', include('contacts.urls')),
+    path('api/campaigns/', include('campaigns.urls')),
 ]
