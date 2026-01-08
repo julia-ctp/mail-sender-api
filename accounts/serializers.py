db4049b563991db9cb4e12a998b0f4137e13e51a
julia-ctp <juliactp.santos@gmail.com>
2026-01-07 21:27:55 -0300

    feat: add authentication system with jwt

---
+++ accounts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+import uuid
+
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "date_joined")
+        read_only_fields = ("id", "date_joiend")
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ("email", "password")
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data, username=str(uuid.uuid4()))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = "email"
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import UserRegisterView, MeView, LoginView
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", UserRegisterView.as_view()),
+    path("me/", MeView.as_view()),
+    path("login/", LoginView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+]
