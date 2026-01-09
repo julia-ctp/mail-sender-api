ffc0584f0b34fb16b721a9eba0a4cc788f516d80
julia-ctp <juliactp.santos@gmail.com>
2026-01-09 18:06:32 -0300

    feat: add contacts CRUD endpoints

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+
+class ContactViewSet(ModelViewSet):
+    serializer_class = ContactSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Contact.objects.filter(user=self.request.user, is_active=True)
+
+    def perform_create(self, serializer):
+        contact = Contact.objects.create_or_reactivate(
+            user=self.request.user,
+            **serializer.validated_data
+        )
+        serializer.instance = contact
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
