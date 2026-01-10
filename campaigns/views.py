88b8c51e3a9a0e5ad7303585207a5c876389e925
julia-ctp <juliactp.santos@gmail.com>
2026-01-10 16:39:21 -0300

    feat: add campaigns CRUD endpoints

--- campaigns/views.py
+++ campaigns/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Campaign
+from .serializers import CampaignSerializer
 
-# Create your views here.
+
+class CampaignViewset(ModelViewSet):
+    serializer_class = CampaignSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Campaign.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user, 
+            sender_email=self.request.user.email
+        )
+
